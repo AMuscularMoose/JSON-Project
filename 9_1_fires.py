eb8fe05384a6ebc7fe80186af9791641c1c1f5ba
Justin Jang <T_Jang@baylor.edu>
2021-03-03 23:22:02 -0600

    9-1 fires created

---
+++ 9_1_fires.py
@@ -0,0 +1,44 @@
+import json
+
+infile = open('US_fires_9_1.json', 'r')
+
+eq_data = json.load(infile)
+
+lats,lons,brites = [],[],[]
+
+for eq in eq_data:
+    lat = eq['latitude']
+    lon = eq['longitude']
+    brt = eq['brightness']
+
+    lats.append(lat)
+    lons.append(lon)
+    brites.append(brt)
+
+print(lats[:10])
+print(lons[:10])
+print(brites[:10])
+
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [
+    {
+        'type': 'scattergeo',
+        'lon': lons,
+        'lat': lats,
+        'marker': {
+            'size':[5*brt for brt in brites],
+            'color':brites,
+            'colorscale': 'Viridis',
+            'reversescale': True,
+            'colorbar':{'title':'Magnitude'},
+        },
+    }
+]
+
+my_layout = Layout(title = "US Fires - 9/1/2020 through 9/13/2020")
+
+fig = {'data':data, 'layout':my_layout}
+
+offline.plot(fig,filename = 'US_fires_9_1.html')
